6d3c1b718516f85fc730faab216d7048d2b8a936
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-05 16:54:47 +0100

    Update app.py

--- RaspberryPi & Webserver/app.py
+++ RaspberryPi & Webserver/app.py
@@ -14,4 +14,4 @@ def display_dataframe():
     return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
