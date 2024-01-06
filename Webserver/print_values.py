b652e082369faa6af9767fcf36aac65f55c9b3c9
felixmau <fmau@posteo.de>
2024-01-06 13:03:48 +0100

    print serial data

--- Webserver/print_values.py
+++ Webserver/print_values.py
@@ -1,7 +1,11 @@
 import serial
 
+print("Starting...")
+
 # Serial communication setup for Raspberry Pi
 ser = serial.Serial('/dev/ttyACM0', 9600)  # Adjust the port and baud rate accordingly
+
+print("Connection established")
 
 try:
     while True:
