c60ca40d21deed609f8f8fae605171cd626e29c1
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-22 14:58:59 +0100

    Update read_serial.py

--- Webserver/read_serial.py
+++ Webserver/read_serial.py
@@ -10,6 +10,15 @@ data = pd.DataFrame(columns=['SolarCurrent', 'SolarVoltage', 'WindCurrent', 'Win
                              'SolarRadiation', 'Temperature'])
 data.to_csv("arduino_data.csv", sep=";", index_label="timestamp")
 
+
+
+def get_fake_data():
+    return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
+
+def check_viablitiy(lines: list):
+  print(len(lines)
+  return 1
+
 def get_data():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=4) 
     lines = ser.readlines()
@@ -17,8 +26,6 @@ def get_data():
     decoded_lines = [line.decode() for line in lines]
     print(decoded_lines)
    #todo: rearrange lines to fit some layout 
+    check_viablitiy(decoded_lines)
     
     return "Yes"
-
-def get_fake_data():
-    return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
