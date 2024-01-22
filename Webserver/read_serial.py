703f2caa1461647681b51f2aa7f3b2c185e111ce
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-22 15:01:20 +0100

    Update read_serial.py

--- Webserver/read_serial.py
+++ Webserver/read_serial.py
@@ -10,8 +10,6 @@ data = pd.DataFrame(columns=['SolarCurrent', 'SolarVoltage', 'WindCurrent', 'Win
                              'SolarRadiation', 'Temperature'])
 data.to_csv("arduino_data.csv", sep=";", index_label="timestamp")
 
-
-
 def get_fake_data():
     return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
 
