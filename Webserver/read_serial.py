48a672c04f9a951138f7c02f41071001e25c912e
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-22 13:28:12 +0100

    Update read_serial.py

--- Webserver/read_serial.py
+++ Webserver/read_serial.py
@@ -13,8 +13,7 @@ def get_data():
     ser = serial.Serial('/dev/ttyACM0', 9600)  # Adjust the port and baud rate accordingly
 
     lines = [ser.readline() for _ in range(12)]
-    lines.join(";")
-    return lines
+    return ';'.join(lines)
 
 def get_fake_data():
     return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
