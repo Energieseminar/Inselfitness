b26be9a50109c28e2f6ea1d98767d197e7d95f48
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-22 15:02:04 +0100

    Update read_serial.py

--- Webserver/read_serial.py
+++ Webserver/read_serial.py
@@ -14,7 +14,7 @@ def get_fake_data():
     return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
 
 def check_viablitiy(lines: list):
-    print(len(lines)
+    print(len(lines))
     return True
 
 def get_data():
