5bc031a4a71464f705c120e35767f9b2e80f5ac4
FelixMau <98911066+FelixMau@users.noreply.github.com>
2024-01-22 13:20:24 +0100

    Update read_serial.py

--- Webserver/read_serial.py
+++ Webserver/read_serial.py
@@ -12,7 +12,7 @@ data.to_csv("arduino_data.csv", sep=";", index_label="timestamp")
 def get_data():
     ser = serial.Serial('/dev/ttyACM0', 9600)  # Adjust the port and baud rate accordingly
 
-    return ser.readline().decode()
+    return [while line:=ser.readline() line]
 
 def get_fake_data():
     return  "SolarCurrent,100.50,SolarVoltage,24.00,WindCurrent,5.30,WindVoltage,12.00,BatteryVoltage,48.00,BiogasPowerDraw,120.00,InverterPowerConsumption,30.00,WindSpeed,8.50,SolarRadiation,800.00,Temperature,25.00".split(",")
